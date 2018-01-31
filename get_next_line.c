d91c8aa909abfc24f79fdcb49e76049b4828fa1f
Vuyani NXELE <vnxele@e4r17p10.wethinkcode.co.za>
2018-01-31 09:38:31 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -5,82 +5,90 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vnxele <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/30 17:40:37 by vnxele            #+#    #+#             */
-/*   Updated: 2017/12/02 13:04:22 by vnxele           ###   ########.fr       */
+/*   Created: 2018/01/30 09:24:13 by vnxele            #+#    #+#             */
+/*   Updated: 2018/01/30 16:01:17 by vnxele           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_line(char **line, char **cache, int j)
+void	ret_line(t_static *head, char buff[], char **line)
 {
-	int i;
-	char *tmp;
-
-	i = 0;
-	int k = ft_strlen(*line);
-	tmp = *line;	
-	*line = ft_strnew(k);
-	while(tmp[i] != '\n')
-	{
-		(*line)[i] = tmp[i];
-		i++;
-	}
-	return(1);
+	head->cache = ft_strdup(ft_strchr(buff, '\n'));
+	head->temp = ft_strsub(buff, 0, ft_strlen(buff) - ft_strlen(head->cache));
+	head->temp2 = *line;
+	*line = ft_strjoin(*line, head->temp);
+	free(head->temp);
+	free(head->temp2);
+	head->temp = head->cache;
+	head->cache = ft_strsub(head->cache, 1, ft_strlen(head->cache));
+	free(head->temp);
 }
 
-int		get_next_line(int fd, char **line)
+int		get_line(t_static *head, int fd, char **line)
 {
-	int j;
-	static char *cache;
-	char		*buff;
+	int		ret;
+	char	buff[BUFF_SIZE + 1];
 
-	buff = (char*)malloc(sizeof(char) * BUFF_SIZE + 1);
-	if (fd < 0 || *line == NULL)
-		return (-1);
-	if (*line)
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		int k = ft_strlen(*line);
-		ft_bzero(*line, k);
+		buff[ret] = '\0';
+		if (ft_strchr(buff, '\n'))
+		{
+			ret_line(head, buff, line);
+			return (1);
+		}
+		head->temp = *line;
+		*line = ft_strjoin(*line, buff);
+		free(head->temp);
 	}
-	//if (cache)
-	//{
-	//	*line = ft_strcat(cache, *line);
-	//	return(1);
-	//}
-	while ((j = read(fd, buff, BUFF_SIZE)) > 0)
-	{
-		if (buff[0] == '\n')
-			return(1);
+	if (ret < 0)
+		return (ret);
+	if (**line)
+		return (1);
+	return (0);
+}
 
-		buff[j] = '\0';
-		*line = ft_strcat(*line, buff);
+int		get_next_line(const int fd, char **line)
+{
+	static	t_static	head;
+
+	if (!(*line = ft_strnew(0)) || fd < 0)
+		return (-1);
+	if (head.cache)
+	{
+		free(*line);
+		*line = ft_strdup(head.cache);
 		if (ft_strchr(*line, '\n'))
 		{
-			//if (cache)
-			//{
-			//	printf("Cache: %s\n", cache);
-			//	get_line(line, &cache, j);
-			//	*line = ft_strcat(cache, *line);
-			//	return(1);
-			//}
-			cache = ft_strchr(*line, '\n');
-			return(get_line(line, &cache, j));
+			free(head.cache);
+			head.cache = ft_strdup(ft_strchr(*line, '\n'));
+			head.temp = *line;
+			*line = ft_strsub(*line, 0, ft_strlen(*line)
+					- ft_strlen(head.cache));
+			free(head.temp);
+			head.temp = head.cache;
+			head.cache = ft_strsub(head.cache, 1, ft_strlen(head.cache));
+			free(head.temp);
+			return (1);
 		}
+		if (head.cache)
+			free(head.cache);
 	}
-	return(0);
+	return (get_line(&head, fd, line));
 }
 
 int		main()
 {
 	char *line;
-	int  fd;
+	int fd;
 
-	fd = open("poem.txt", O_RDONLY);
-	while(get_next_line(fd, &line) > 0)
+	fd = open("text.txt", O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
 	{
 		ft_putendl(line);
-		//ft_putendl("_______");
+		free(line);
 	}
+	free(line);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,23 @@
 /*   By: vnxele <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/29 11:17:29 by vnxele            #+#    #+#             */
-/*   Updated: 2017/12/02 12:52:48 by vnxele           ###   ########.fr       */
+/*   Updated: 2018/01/30 15:17:16 by vnxele           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 64
 # include "libft/libft.h"
 # include <fcntl.h>
+
+typedef struct	s_static
+{
+	char		*cache;
+	char		*temp;
+	char		*temp1;
+	char		*temp2;
+}				t_static;
 
 int				get_next_line(int fd, char **line);
 
